a4da0a8d0ba3bb7283f2d0437b6b14292ad92e1d
QuanLe Leo <leho.vietquan@gmail.com>
2020-12-22 17:59:30 -0500

    index file, highscores file, questions, quizfunctions and scorefunction created

---
+++ questions.js
@@ -0,0 +1,28 @@
+// questions *****************************************************
+var questions = [
+    {
+        title: "What is the name of Naruto's Author?",
+        choices: ["Masashi Kishimoto", "Sasuke Uchiha", "Naruto Uzumaki", "Sakura Haruno"],
+        answer: "Masashi Kishimoto"
+    },
+    {
+        title: "What is the name of the cat that has a magic pocket on its belly?",
+        choices: ["Hello-Kitty", "Puss-in-Boots", "Do-ra-e-mon", "Totoro"],
+        answer: "Do-ra-e-mon"
+    },
+    {
+        title: "What is One-Piece?",
+        choices: ["literally what its mean", "a piece of pie", "a pirate treasure", "Both 1 and 2"],
+        answer: "a pirate treasure"
+    },
+    {
+        title: "Who/what is Black Jack?",
+        choices: ["a famous actor", "a famous card-dealer", "a card game", "a famous doctor"],
+        answer: "a famous doctor"
+    },
+    {
+        title: "Which of the following is the genre meaning 'pervert'?",
+        choices: ["ecchi", "yaoi", "yuri", "hentai"],
+        answer: "hentai"
+    }
+]
---
+++ quizfunctions.js
@@ -0,0 +1,174 @@
+// variables to keep track of quiz state
+var currentQuestionIndex = 0;
+var time = questions.length * 20;
+var timerId;
+
+// variables to reference DOM elements....................................
+var timeEl = document.querySelector("#time");
+var startBtn = document.querySelector("#startButton");
+var submitBtn = document.querySelector("#submit-button");
+var titleScreen = document.querySelector("#title-section");
+var quizScreen = document.querySelector("#quiz-section");
+var highScoreScreen = document.querySelector("#highscore-section");
+var highScoreDisplay = document.querySelector("#highscore-display-section");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
+
+var questionsEl = document.querySelector("#question");
+var choicesEl = document.querySelector("#choices");
+
+
+//create a function to start the game
+function startQuiz() {
+  // hide start screen
+  titleScreen.setAttribute("class", "hide");
+
+  // un-hide questions section
+  quizScreen.setAttribute("class", "show");
+
+  // start timer
+  timerId = setInterval(tick, 1000);
+
+  // show starting time
+  timeEl.textContent = time;
+
+  getQuestion();
+}
+
+//create a second taken off of a clock
+function tick() {
+  // update time
+  time--;
+  timeEl.textContent = time;
+
+  // check if user ran out of time
+  if (time <= 0) {
+    quizEnd();
+  }
+}
+
+function getQuestion() {
+  // get current question object from array
+  var currentQuestion = questions[currentQuestionIndex];
+
+  // update title with current question
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+
+  // clear out any old question choices
+  choicesEl.innerHTML = "";
+
+  // loop over choices
+  currentQuestion.choices.forEach(function (choice, i) {
+    // create new button for each choice
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+
+    choiceNode.textContent = i + 1 + ". " + choice;
+
+    // attach click event listener to each choice
+    choiceNode.onclick = questionClick;
+
+    // display on the page
+    choicesEl.appendChild(choiceNode);
+  });
+}
+
+// click on question answer either generate new question or end quiz if final question, and deduct time for answering wrong
+function questionClick() {
+  // check if user guessed wrong
+  if (this.value !== questions[currentQuestionIndex].answer) {
+    // penalize time
+    time -= 10;
+
+    if (time < 0) {
+      time = 0;
+    }
+
+    // display new time on page
+    timeEl.textContent = time;
+
+
+    feedbackEl.textContent = "Eh-eh Better luck next question!";
+  } else {
+
+    feedbackEl.textContent = "Ding-ding u're right!";
+  }
+
+  // flash right/wrong feedback on page for half a second
+  feedbackEl.setAttribute("class", "feedback");
+  setTimeout(function () {
+    feedbackEl.setAttribute("class", "feedback hide");
+  }, 1000);
+
+  // move to next question
+  currentQuestionIndex++;
+
+  // check if we've run out of questions
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+}
+
+
+// end the quiz function
+function quizEnd() {
+  // stop timer
+  clearInterval(timerId);
+
+  // show end screen
+  var highscoreSectionEl = document.querySelector("#highscore-section");
+  highscoreSectionEl.setAttribute("class", "show");
+
+  // show final score
+  var finalScoreEl = document.querySelector("#final-score");
+  finalScoreEl.textContent = time;
+
+  // hide questions section
+  quizScreen.setAttribute("class", "hide");
+}
+
+// function for saving highscore
+function saveHighscore() {
+  // get value of input box
+  var initials = initialsEl.value.trim();
+
+  // make sure value wasn't empty
+  if (initials !== "") {
+    // get saved scores from localstorage, or if not any, set to empty array
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    // format new score object for current user
+    var newScore = {
+      score: time,
+      initials: initials
+    };
+    console.log(newScore);
+    // save to localstorage
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    // redirect to next page
+    window.location.href = "highscore.html";
+  }
+
+}
+
+function checkForEnter(event) {
+  // "13" represents the enter key
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
+}
+
+// user clicks button to submit initials
+submitBtn.onclick = saveHighscore;
+
+// user clicks button to start quiz
+startBtn.onclick = startQuiz;
+
+initialsEl.onkeyup = checkForEnter;
